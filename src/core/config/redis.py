b4ae949809763fbdf70319f5921699ceb6eee4f4
Anton Iusupov <antonyusupov@outlook.com>
2026-02-18 16:45:36 +0400

    No redis password allowed

--- src/core/config/redis.py
+++ src/core/config/redis.py
@@ -9,13 +9,13 @@ class RedisConfig(BaseConfig, env_prefix="REDIS_"):
     host: str = "remnashop-redis"
     port: int = 6379
     name: str = "0"
-    password: SecretStr
+    password: SecretStr | None = None
 
     @property
     def dsn(self) -> str:
         return RedisDsn.build(
             scheme="redis",
-            password=self.password.get_secret_value(),
+            password=self.password.get_secret_value() if self.password else None,
             host=self.host,
             port=self.port,
             path=self.name,
@@ -23,6 +23,9 @@ class RedisConfig(BaseConfig, env_prefix="REDIS_"):
 
     @field_validator("password")
     @classmethod
-    def validate_redis_password(cls, field: SecretStr, info: FieldValidationInfo) -> SecretStr:
-        validate_not_change_me(field, info)
+    def validate_redis_password(
+        cls, field: SecretStr | None, info: FieldValidationInfo
+    ) -> SecretStr | None:
+        if field is not None:
+            validate_not_change_me(field, info)
         return field
